99999179a2e53b03256806dc64bd0c885e65d24d
SHAURYA369 <53884731+SHAURYA369@users.noreply.github.com>
2023-06-28 00:46:35 +0530

    Created the primary and secondary contacts

--- index.js
+++ index.js
@@ -9,26 +9,65 @@ app.use(bodyParser.json());
 // // Create a Sequelize instance to connect to the database
 const sequelize = new Sequelize(process.env.YOUR_DB_URI);
 // Define the Contact model
-class Contact extends Model {}
+class Contact extends Model { }
 Contact.init(
-  {
-    phoneNumber: DataTypes.STRING,
-    email: DataTypes.STRING,
-    linkedId: DataTypes.INTEGER,
-    linkPrecedence: DataTypes.ENUM('primary', 'secondary'),
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE,
-    deletedAt: DataTypes.DATE,
-  },
-  {
-    sequelize,
-    modelName: 'Contact',
-    tableName: 'Contact',
-  }
+    {
+        phoneNumber: DataTypes.STRING,
+        email: DataTypes.STRING,
+        linkedId: DataTypes.INTEGER,
+        linkPrecedence: DataTypes.ENUM('primary', 'secondary'),
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE,
+        deletedAt: DataTypes.DATE,
+    },
+    {
+        sequelize,
+        modelName: 'Contact',
+        tableName: 'Contact',
+    }
 );
+
+app.post('/identify', async (req, res) => {
+    const { email, phoneNumber } = req.body;
+
+    // Find the primary contact based on email or phone number
+    const primaryContact = await Contact.findOne({
+        where: {
+            [Op.or]: [{ email }, { phoneNumber }],
+        },
+        order: [['createdAt', 'ASC']]
+    });
+
+    if (primaryContact) {
+        const primaryContactId = primaryContact.linkedId || primaryContact.id;
+        const newContact = await Contact.create({
+            email,
+            phoneNumber,
+            linkedId: primaryContactId,
+            linkPrecedence: 'secondary',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        })
+
+
+    } else {
+        // Create a new primary contact
+        const newContact = await Contact.create({
+            email,
+            phoneNumber,
+            linkPrecedence: 'primary',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        });
+
+
+    }
+});
+
 // Sync the models with the database and start the server
+
 sequelize.sync().then(() => {
     app.listen(3000, () => {
-      console.log('Server started on port 3000');
+        console.log('Server started on port 3000');
     });
-  });
+});

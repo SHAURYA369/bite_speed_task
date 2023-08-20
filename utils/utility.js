a3b2cbf499126b4c043afc4c09caed84da2c7ba8
SHAURYA369 <53884731+SHAURYA369@users.noreply.github.com>
2023-08-20 21:49:34 +0530

    Added null checks

--- utils/utility.js
+++ utils/utility.js
@@ -1,8 +1,12 @@
  function validateEmail(email) {
+    if(email===null)
+    return true;
     const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     return emailRegex.test(email);
 }
 function validatePhoneNumber(phoneNumber) {
+    if(phoneNumber===null)
+    return true;
     const phoneNumberRegex = /^[0-9]{10}$/;
     return phoneNumberRegex.test(phoneNumber);
 }
